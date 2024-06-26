4b3ad25f699c4e4cf76ea6606745b83d29263c05
Andrey Golikov <golickovandrey@gmail.com>
2024-06-26 23:50:44 +0300

    Base serialization using methods

---
+++ Models/Datagram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Xml.Serialization;
+
+namespace XmlParsing.Models
+{
+    //[Serializable()]
+    [XmlRoot(ElementName = "datagram")]
+    public class Datagram
+    {
+        #region Attributes
+        
+        [XmlAttribute("id")]
+        public Guid Id { get; set; }
+
+        #endregion
+
+
+        #region NestedTags
+        
+        [XmlElement(Type = typeof(Tag11))]
+        public ArrayList tag11;
+
+        #endregion
+    }
+
+    public class Tag11
+    {
+        #region NestedTags
+        
+        [XmlElement(ElementName = "tag12")]
+        public Tag12 Tag12;
+
+        [XmlElement(ElementName = "tag22")]
+        public DateTime Tag22;
+
+        #endregion
+    }
+
+    public class Tag12
+    {
+        #region Attributes
+
+        [XmlAttribute("stringAttr1")]
+        public string StringAttr1 { get; set; }
+
+        #endregion
+
+
+        #region Value
+
+        [XmlText(typeof(Guid))]
+        public Guid Value;
+
+        #endregion
+    }
+}
+
+/*
+<datagram>
+    <tag11 master_id="3e68fc8b-5de7-4fd8-8f4e-69752731f8fd" firm_id="KBP">
+        <tag12 stringAttribute="stringAttributeValue">11ed1b93-f619-4c73-ba07-46d75670c445</tag12>
+    </tag11>
+    <tag11 master_id="e2652b72-4048-4060-affa-ab09a1566279" firm_id="TSKIB">
+        <tag12 stringAttribute="stringAttributeValue">9cbde049-0720-411a-aaff-c4cd75d245d4</tag12>
+    </tag11>
+</datagram>
+ */
--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using XmlParsing.Models;
 
 namespace XmlParsing
 {
@@ -10,6 +10,22 @@ namespace XmlParsing
     {
         static void Main(string[] args)
         {
+            string inputFilePath = Path.GetFullPath(@".\..\..\data\example.xml");
+            Console.WriteLine($"inputFilePath: {inputFilePath}{Environment.NewLine}");
+
+            XElement document = XElement.Load(inputFilePath);
+            Console.WriteLine($"document:{Environment.NewLine}{document}");
+
+            Datagram datagram = XmlToDatagram(document);
+
+            Console.ReadKey();
+        }
+
+        private static Datagram XmlToDatagram(XElement xml)
+        {
+            StringReader reader = new StringReader(xml.ToString());
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Datagram));
+            return (Datagram)xmlSerializer.Deserialize(reader);
         }
     }
 }
